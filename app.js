be68165bcd762f61ae1f053e21a49222cc77fc08
hajratariqi <hajra.ashrafqadri@gmail.com>
2024-10-22 00:48:48 +0500

    solve quiz questions for practice

---
+++ app.js
@@ -0,0 +1,14 @@
+let i = 10;
+do {
+    i--;
+    console.log(i);1
+} while (i > 10);
+
+
+
+let count = 0;
+while (count < 5) {
+    if (count === 2) break;
+    console.log(count);
+    count++;
+}
